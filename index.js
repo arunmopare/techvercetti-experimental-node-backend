18214169df59f61aca3bc3dd97ac68ee3808efad
Arun Mopare <arunmopare546@gmail.com>
2022-04-17 13:18:19 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+var cors = require("cors");
+
+const port = 3000
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
